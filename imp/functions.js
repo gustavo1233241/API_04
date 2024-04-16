3a3c44476455ac2551327284fec151913bf37b99
Your Name <you@example.com>
2024-04-16 16:13:23 -0300

    s

---
+++ imp/functions.js
@@ -0,0 +1,20 @@
+function response(sts,msg,aftrows, data = null){
+
+
+return {
+
+status:sts,
+message:msg,
+affected_rows:aftrows,
+data:data,
+timestamp: new Date().getTime()
+
+}
+
+}
+
+module.exports ={
+
+response
+
+}
